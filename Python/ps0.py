9376e88f8e3b29c46c5ce4caba776406cbeea628
gringogidget <kristinagroeger@gmail.com>
2014-12-01 15:53:07 -0500

    MIT Courseware

---
+++ Python/ps0.py
@@ -0,0 +1,9 @@
+# Problem Set 0
+# Name: Kristina Groeger
+# Collaborators: None
+# Time Spent: 3:30
+
+x = raw_input('What is your name?') 
+y = raw_input('What is your birthday?') 
+print ('Your name is ' + x)
+print ('Your birthday is ' +y)
