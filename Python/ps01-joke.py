5060b970f5a3f436700dbe4e3167fe1da98f2509
gringogidget <kristinagroeger@gmail.com>
2014-12-01 15:54:02 -0500

    haha I'm so funny

---
+++ Python/ps01-joke.py
@@ -0,0 +1,6 @@
+#a Python joke
+
+x = raw_input('What is your name?') 
+y = raw_input('What is your birthday?') 
+print ('Only idiots are called ' + x)
+print ('Why dont you go jump in a lake next ' +y)
