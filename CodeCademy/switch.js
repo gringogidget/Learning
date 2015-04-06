75655cb18e1ffab8d6c646185cdce21f7f07d5a6
gringogidget <kristinagroeger@gmail.com>
2015-04-06 09:24:54 -0400

    Switches

---
+++ CodeCademy/switch.js
@@ -0,0 +1,23 @@
+var answer = prompt("Do you have to poop?");
+
+switch(answer) {
+  
+  case 'yes':
+    console.log("I'll hold your hand.");
+    break;
+  
+  case 'maybe':
+    console.log("Try farting a bit more.");
+    break;
+    
+  case 'of course':
+    console.log("I think you've got this.");
+    break;
+
+  case 'no':
+    console.log("Don't worry, it'll come in a few hours.");
+    break;
+    
+    default:
+    console.log("Poopin' rules!");  
+}
