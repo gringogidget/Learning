7dd154bfe6f37b71a075ca05e5de45eabb04edcd
gringogidget <kristinagroeger@gmail.com>
2015-04-11 18:20:50 -0400

    Custom Constructors

--- CodeCademy/constructors.js
+++ CodeCademy/constructors.js
@@ -60,3 +60,25 @@ var holden = new Person("Holden Caulfield", 16);
 
 console.log("sally's species is " + sally.species + " and she is " + sally.age);
 console.log("holden's species is " + holden.species + " and he is " + holden.age);
+
+
+/* In addition to the basic Object constructor, we can define our own custom constructors. These are helpful for two reasons:
+
+We can assign our objects properties through parameters we pass in when the object is created.
+We can give our objects methods automatically.
+These both work to save us time and lines of code when we make objects. */
+
+// 3 lines required to make harry_potter
+var harry_potter = new Object();
+harry_potter.pages = 350;
+harry_potter.author = "J.K. Rowling";
+
+// A custom constructor for book
+function Book (pages, author) {
+    this.pages = pages;
+    this.author = author;
+}
+
+
+var the_hobbit = new Book(320, "J.R.R. Tolkien");
+
