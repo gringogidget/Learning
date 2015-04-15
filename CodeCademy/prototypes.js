4c3bac71092491f365f8957f291f8ba87cc130a6
gringogidget <kristinagroeger@gmail.com>
2015-04-15 08:50:53 -0400

    More on inheritance
    Practise / lesson

--- CodeCademy/prototypes.js
+++ CodeCademy/prototypes.js
@@ -69,3 +69,60 @@ Cat.prototype.meow = function () {
     console.print("Meow!");
     };
 
+
+// Create an animal class with two properties. The Animal constructor should
+// have two arguments, name and numLegs
+// Change the prototype of Animal and add method sayName.
+
+
+// create your Animal class here
+function Animal(name, numLegs){
+this.name = name;
+this.numLegs = numLegs;
+}
+
+// create the sayName method for Animal
+Animal.prototype.sayName = function() {
+console.log("Hi my name is " + this.name);
+};
+
+// provided code to test above constructor and method
+var penguin = new Animal("Captain Cook", 2);
+penguin.sayName();
+
+
+
+/* Create a brand new Penguin class constructor. 
+A penguin is an animal so it should also have the 
+name and numLegs properties as well as a sayName 
+method that prints the same thing as Animal's sayName method.
+
+We're not done with animals yet, so we have still 
+included the Animal constructor and its sayName method. 
+The last two lines test your Penguin code.
+*/
+
+ function Animal(name, numLegs) {
+    this.name = name;
+    this.numLegs = numLegs;
+}
+Animal.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
+};
+
+
+// create a Penguin constructor here
+function Penguin(name, numLegs){
+    this.name = name;
+    this.numLegs = numLegs;
+}
+
+// create a sayName method for Penguins here
+Penguin.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
+};
+
+
+// our test code
+var theCaptain = new Penguin("Captain Cook", 2);
+theCaptain.sayName();
