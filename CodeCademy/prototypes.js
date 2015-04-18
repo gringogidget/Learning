20dfd316c05f36a56f26606d4b1b8107daba41cc
Aprile Elcich <info@aprileelcich.com>
2015-04-18 19:36:54 -0400

    Class definition

--- CodeCademy/prototypes.js
+++ CodeCademy/prototypes.js
@@ -200,3 +200,32 @@ var emperor = new Emperor("bob");
 
 console.log(emperor.numLegs);
 
+
+//Complete the console.log statement to print appropriate responses
+//(If a property is not defined for a class, this class's prototype chain will
+  //be traversed upwards until one is found (or not) in a parent (higher) class)
+
+
+function Animal(name, numLegs) {
+this.name = name;
+this.numLegs = numLegs;
+this.isAlive = true;
+}
+function Penguin(name) {
+this.name = name;
+this.numLegs = 2;
+}
+function Emperor(name) {
+this.name = name;
+this.saying = "Waddle waddle";
+}
+
+// set up the prototype chain
+Penguin.prototype = new Animal();
+Emperor.prototype = new Penguin();
+
+var myEmperor = new Emperor("Jules");
+
+console.log(myEmperor.saying); // should print "Waddle waddle"
+console.log(myEmperor.numLegs); // should print 2
+console.log(myEmperor.isAlive); // should print true
