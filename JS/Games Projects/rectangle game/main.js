9676c47a14f4d1306267a3a3153ba9c12fd9dd57
gringogidget <kristinagroeger@gmail.com>
2015-05-15 07:19:01 -0400

    WIP basic rectangle game
    canvas/JS/HTML5

---
+++ JS/Games Projects/rectangle game/main.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("canvas");
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
+canvas.style.width = canvas.width + "px";
+canvas.style.height: = canvas.height + "px";
+
+var ctx = canvas.getContext('2d');
+
