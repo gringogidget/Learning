e073680560c8448a50def4e218123e3e2758400f
gringogidget <kristinagroeger@gmail.com>
2015-05-29 23:54:36 -0400

    02

---
+++ JS/Code School/02ForLoop.js
@@ -0,0 +1,5 @@
+/* For loops have two semicolons inside that break up the for loops paramaters
+
+for ( *initialization* ; *condition check if loop will continue *; *do this after each loop*) {
+	*in each loop do this code*
+}
