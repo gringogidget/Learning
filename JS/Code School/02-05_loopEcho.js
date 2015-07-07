125c234874ecba9872b2fede73090d8ffb1553a4
gringogidget <kristinagroeger@gmail.com>
2015-07-06 20:53:35 -0400

    complex conditionals

--- JS/Code School/02-05_loopEcho.js
+++ JS/Code School/02-05_loopEcho.js
@@ -59,5 +59,34 @@ for (var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
   console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
 }
 
+//Exercise #2
+//In one for loop, show only the even numbered turbines.
+	// W a set of complex conditional statements inside the loop
+	//construct a way to only turn on even numbered turbines.
+	//	Generators 1 through 4 produce 62 MW.
+	//	Generators 5 through 19 produce 124 MW.
+	//Using the following format:
+	// 	Generator #1 is off.
+	// 	Generator #2 is on, adding 62 MW, for a total of 62 MW!
 
+var totalGen = 19;
+var totalMW = 0;
+
+for(var currentGen =1; currentGen <=totalGen; currentGen ++){
+  
+  if (currentGen <=4 && currentGen %2 == 0) {
+    var firstsGenMW = 62;
+    totalMW = (totalMW + firstsGenMW);
+    console.log("Generator #" + currentGen  + " is on, adding " + firstsGenMW + " MW, for a total of " + totalMW + " MW!");
+  
+  } else if (currentGen >=5 && currentGen %2 == 0){
+    var secondGenMW = 124;
+    totalMW = (totalMW + secondGenMW);
+    console.log("Generator #" + currentGen  + " is on, adding " + secondGenMW + " MW, for a total of " + totalMW + " MW!");
+  
+  } else {
+  console.log("Generator #" + currentGen  + " is off.");
+  }
+}
+}
 
--- JS/Code School/test.js
+++ JS/Code School/test.js
@@ -1,83 +1,16 @@
-var numSheep = 4;
-var monthNumber = 1;
-var monthsToPrint = 12;
-
-while (monthNumber <= monthsToPrint) {
-
-numSheep = numSheep*4;
-
-console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!")
-
-monthNumber++;
-
-}
-
-// every month, the population multiplies by 4
-// print 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var currentGen = 1;
 var totalGen = 19;
 var totalMW = 0;
 
-// while loop handles first 4 currentGen
-while (currentGen <= 4) {
-  // the first four output 62 megawatts
-  totalMW += 62;
-  console.log("Generator #" + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
-  currentGen++;
-}
-
-// currentGen var now starts at 5 and goes up to 15, increasing by 1
-for (currentGen >= 5; currentGen <= totalGen; currentGen++) {
-  //totalMW increases each increment by 124
-  totalMW += 124;
-
-  //print console new info
-  console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
-}
-
-var currentGen = 1;
-var totalGen = 19;
-var totalMW = 0;
-
-while (currentGen <= 4) {
-  totalMW += 62;
-
-  console.log ("Generator #"+ currentGen + " is on, adding 62 MW, for a total of "+ totalMW +" MW!");
-
-  currentGen++;
-}
-
-for(currentGen >= 5; currentGen <= totalGen; currentGen++) {
-  totalMW += 124;
-
-  console.log ("Generator #"+ currentGen + " is on, adding 124 MW, for a total of "+ totalMW + " MW!");
+for(var currentGen =1; currentGen <=totalGen; currentGen ++){
+  if (currentGen <=4 && currentGen %2 == 0) {
+    var firstsGenMW = 62;
+    totalMW = (totalMW+firstsGenMW);
+    console.log("Generator #"+ currentGen  +" is on, adding "+ firstsGenMW +" MW, for a total of "+ totalMW +" MW!");
+  }else if(currentGen >=5 && currentGen %2 == 0){
+    var secondGenMW = 124;
+    totalMW = (totalMW+secondGenMW);
+    console.log("Generator #"+ currentGen  +" is on, adding "+ secondGenMW +" MW, for a total of "+ totalMW +" MW!");
+  }else{
+  console.log("Generator #"+ currentGen  +" is off.");
+  }
 }
