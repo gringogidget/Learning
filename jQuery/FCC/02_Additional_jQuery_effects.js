ce8df472509f70973966e436373a6f4fdf8a1a4e
Kristina Groeger <me@kristinagroeger.com>
2015-07-12 15:09:50 -0400

    Additional jQuery effects

---
+++ jQuery/FCC/02_Additional_jQuery_effects.js
@@ -0,0 +1,72 @@
+// Exercise 1: 
+// Use necessary jQuery to .hide your div when the document is ready.
+
+$(document).ready(function() {
+    $('div').hide();
+});
+
+
+// Exercise 2:
+// .animate() practice: 
+// Make the image go down by 100px and at 1 second:
+
+$(document).ready(function(){
+    $('img').animate({top: "+=100px"}, 1);
+});
+
+
+// Exercise 3: 
+// Using jQuery UI make the image explode.
+
+//HTML:
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Krypton Redux</title>
+		<link rel='stylesheet' type='text/css' href='stylesheet.css'/>
+        <script type='text/javascript' src='script.js'></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+	</head>
+	<body>
+		<div></div>
+	</body>
+</html>
+
+//jQuery:
+
+$(document).ready(function() {
+    $('div').click(function() {
+        $(this).effect('explode');
+    });
+});
+
+
+// Exercise 4:
+// Bounce image 3x within 500 milliseconds
+
+$(document).ready(function() {
+    $('div').click(function() {
+        $(this).effect('bounce', {times:3}, 500);
+    });
+});
+
+
+// Exercise 5: 
+// Make the image slide
+
+$(document).ready(function() {
+    $('div').click(function() {
+        $(this).effect('slide');
+    });
+});
+
+
+// Exercise 6:
+// Call the .draggable() funciton on '#car'
+
+$(document).ready(function() {
+    $(function() {
+        $("#car").draggable();
+    });
+});
