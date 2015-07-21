754f3fbc41a14b3c45bb676f5d388b8c8826c5e5
Kristina Groeger <me@kristinagroeger.com>
2015-07-20 20:04:16 -0400

    reverseString()

---
+++ JS/FCC/bonfireChallenges.js
@@ -0,0 +1,18 @@
+// Reverse the provided string:
+
+function reverseString(str) {
+  return str;
+}
+
+reverseString('hello');
+
+// Answer:
+
+function reverseString(str) {  
+    return str.split('').reverse().join(''); 
+} 
+reverseString('hello');  
+
+
+
+/**********
