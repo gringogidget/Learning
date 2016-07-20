2c1e4246b91dcf410faeb74de4d9fc23d3a75eeb
Kristina Groeger <kristinagroeger@gmail.com>
2016-07-20 13:04:52 -0400

    Module Solutions
    FCC

---
+++ JS/FCC/Accessing Objects Properties with the Dot Operator.js
@@ -0,0 +1,19 @@
+//There are two ways to access the properties of an object: the dot operator (.) and bracket notation ([]), similar to an array.
+
+//The dot operator is what you use when you know the name of the property you're trying to access ahead of time.
+
+//Here is a sample of using the dot operator (.) to read an object property:
+
+
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
---
+++ JS/FCC/Build JS Objects.js
@@ -0,0 +1,20 @@
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  
+  "name": "Fifi",
+  "legs": 3,
+  "tails": 2,
+  "friends": ["Hydrants!"],
+  
+  
+};
