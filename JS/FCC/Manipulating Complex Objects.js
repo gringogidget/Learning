50a74e71e29e2a1e7e6ed581e75ddbde25df42d2
Kristina Groeger <kristinagroeger@gmail.com>
2016-07-21 08:42:36 -0400

    Learning about JSON

---
+++ JS/FCC/Manipulating Complex Objects.js
@@ -0,0 +1,27 @@
+
+var myMusic = [
+  {
+    "artist": "Gorgoroth",
+    "title": "Under the Sun of Hell",
+    "release_year": 1997,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": false
+  },
+
+ {
+    "artist": "Burzum",
+    "title": "Filosofem",
+    "release_year": 1996,
+    "formats": [ 
+      "Cassette", 
+      "LP" ],
+      "gold": false
+  } 
+
+
+
+];
+
