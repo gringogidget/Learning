b88c42fa9dd75a1979e79aff9d979c914dce5981
Kristina Groeger <kristinagroeger@gmail.com>
2017-10-06 17:08:49 -0400

    👶🏻

--- JS/functions/functions.js
+++ JS/functions/functions.js
@@ -41,3 +41,82 @@ function greet(person1, person2, person3) {
   console.log("hi " + person3);
 }
 greet("harry", "ron", "hermoine");
+
+////////// using the return keyword
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+var city = "paris"; // "paris"
+var capital = capitalize(city); //"Paris" - we can capture the returned value in a variable
+
+////////// function declaration vs. function expression:
+
+// declaration:
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// expression:
+var capitalize = function(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+////////// problem set:
+
+// 1) write a function isEven(), which takes a single numeric argument and returns true if the number is even, and false otherwise
+
+function isEven(evenNumber) {
+  if (evenNumber % 2 === 0) {
+  return ("true")
+} else {
+      return("false")
+    }
+  }
+isEven(5);
+
+// or MUCH SHORTER
+
+function isEven(evenNumber) {
+  return num % 2 === 0;
+}
+isEven(5);
+
+// 2) write a function factorial(), which takes a single numeric argument and returns the factorial of that number
+// a factorial is a number x the numbers below it eg: 4! is 4x3x2x1
+
+function factorial(x) {
+  if (x > 0) {
+    return x * (x -1)
+  } else {
+    if (x === 0) {
+      return ("1")
+    }
+  }
+}
+
+factorial(0);
+
+// or
+
+function isEven(num) {
+  return x % 2 === 0;
+}
+function factorial(num) {
+  var result = 1;
+  for (var i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+}
+factorial(3);
+
+// 3) write a function kebabToSnake(), which takes a single kebab-cased string argument and returns the snake_cased version
+
+function kebabToSnake(changeIt) {
+  if (changeIt === "-") {
+    str.replace("-", "_");
+  }
+}
+kebabToSnake("hello-world");
