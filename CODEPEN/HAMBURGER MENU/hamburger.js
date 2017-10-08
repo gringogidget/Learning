54c5b6c07c9ed177759289144427605fea28b8f1
Kristina Groeger <kristina_groeger@intuit.com>
2017-10-08 10:50:59 -0400

    Reorganizing

--- CODEPEN/HAMBURGER MENU/hamburger.js
+++
@@ -1,7 +0,0 @@
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("myBtn").onclick = function() {myFunction()};
-
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
