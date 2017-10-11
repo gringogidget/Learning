fe3b608c806620db5699c08e8ed61cf8f8f41dfe
Kristina Groeger <kristinagroeger@gmail.com>
2017-10-11 06:34:12 -0400

    💀

--- JS/arrays/arrays02.js
+++ JS/arrays/arrays02.js
@@ -4,6 +4,7 @@
 // if you have 10,000 comments on a reddit post, you would need to print them
 // all. USE a loop: DRY METHOD:
 
+// using for loop:
 var colors = ["red", "orange", "yellow", "green"];
 for (var i = 0; i < colors.length; i++) { // keeps going for however many characters are in the array
   console.log(colors[i]);
@@ -50,3 +51,27 @@ colors.forEach(printColor);  //if you add () it will execute
 // *******
 // green
 // *******
+
+
+// same thing with a while loop (it's longer and hardly ever used)
+
+var count = 0;
+while(count < colors.length) {
+console.log(colors[count]);
+  count++;
+}
+
+
+// exercise:
+// what does the following code print out?
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var colors = ["red", "orange", "yellow", "green"];
+
+
+numbers.forEach(function(color)) { // color is a bad name for this argument
+  if (color % 3 === 0) {
+    console.log(color);
+  }
+});
+// the colors are just there to distract you.
+// it returns multiples of 3: 3, 6, 9.
