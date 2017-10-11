83f0ed6e909a3ac30fa39e49bc0c24ecd01ed5d8
Kristina Groeger <kristinagroeger@gmail.com>
2017-10-11 07:40:33 -0400

    problem sets

---
+++ JS/arrays/arrays03.js
@@ -0,0 +1,31 @@
+////////// problems: //////////
+
+// write a function printReverse() that takes an array as an argument
+// and prints out the elements in the arrayin reverse order without
+// changing the array
+
+printReverse([1, 2, 3, 4]);
+
+
+// write a function isUniform() which takes an array as an argument
+// and returns true if all elements in an array are identical
+
+// hint: check first item and compare it to the rest of them in the loop
+var isUniform = ([1, 1, 1, 1,]); // true
+isUniform([2, 1, 1, 1,]); // false
+
+if (isUniform[0] === true) {
+  console.log("true");
+}
+
+
+// write a function sumArray() that accepts an array of numbers and
+// returns the sum of all numbers in the array
+
+sumArray([1, 2, 3]); // 6
+
+
+// write a function max() that accepts an array of numbers and
+// returns the maximum number in the array
+
+max([1, 2, 3]); // 3
