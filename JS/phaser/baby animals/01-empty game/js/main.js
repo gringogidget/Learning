c49ada5f7a81727466d66bca98e836c7d82e8ef2
Kristina Groeger <kristinagroeger@gmail.com>
2017-11-01 21:40:36 -0400

    Phaser.Game // preload // create // update

--- JS/phaser/baby animals/01-empty game/js/main.js
+++ JS/phaser/baby animals/01-empty game/js/main.js
@@ -1,22 +1,39 @@
-//this game will have only 1 state
-var GameState = {
-  //load the game assets before the game starts
-  preload: function() {
-    this.load.image('background', 'assets/images/background.png');
-    
-  },
-  //executed after everything is loaded
-  create: function() {
-  },
-  //this is executed multiple times per second
-  update: function() {
-  },
-  
+/* Game Setup:
+- Create an empty game with Phaser.Game
+- The code of the game goes into a State
+- Some of the main methods of State are:
+    preload
+    create
+    update
+*/
 
-};
+// physical pixel size of game && if browser doesn't have WEBGL, it will default to canvas
 
-//initiate the Phaser framework
 var game = new Phaser.Game(640, 360, Phaser.AUTO);
 
+// this game is going to be a single state game
+// a state is a JS object with a preload method where all assets are loaded
+// once the game is loaded, the create method is called
+// when create is called, assets can be quickly accessed
+
+var GameState = {
+    preload: function(){
+
+    },
+    
+    create: function() {
+        
+    },
+    // update is used many times per second as long as the game is going, a listener
+    update: function() { 
+    }
+};
+
+// after creating a state, you have to add it to the game
+
 game.state.add('GameState', GameState);
+
+// launch the game
+
 game.state.start('GameState');
+
