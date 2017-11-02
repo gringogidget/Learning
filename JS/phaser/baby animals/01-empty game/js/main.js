b65146abcae8aa073e94e8fd42da5c0aba091290
Kristina Groeger <kristinagroeger@gmail.com>
2017-11-01 22:30:03 -0400

    a whole different theme to this game 👽

--- JS/phaser/baby animals/01-empty game/js/main.js
+++ JS/phaser/baby animals/01-empty game/js/main.js
@@ -5,6 +5,12 @@
     preload
     create
     update
+
+Load the images in the preload method: this.load.image('myImage','assets/images/file.png')
+
+Create a new Sprite to show the image on the screen: this.background = this.game.add.sprite(0,0, 'myImage')
+
+Coordinates start on the top-left of the screen
 */
 
 // physical pixel size of game && if browser doesn't have WEBGL, it will default to canvas
@@ -19,13 +25,17 @@ var game = new Phaser.Game(640, 360, Phaser.AUTO);
 var GameState = {
     preload: function(){
 
+/* this: current object we're in // load: loader object // image: duh // a key eg: 'background'
+    specify the path after the key, */
+
+    this.load.image('background', 'assets/images/background.png') 
     },
     
     create: function() {
-        
+        this.background = this.game.add.sprite(0, 0, 'background') // always gives access to the main game object // add coordinates from top left corner // in this case places the background
     },
-    // update is used many times per second as long as the game is going, a listener
-    update: function() { 
+
+    update: function() { // update is used many times per second as long as the game is going, a listener
     }
 };
 
