be73b95ca00ea7e8bde9ef34494a19e30da2b7fa
Kristina Groeger <kristinagroeger@gmail.com>
2017-11-02 20:50:49 -0400

    👽

--- JS/phaser/baby animals/01-empty game/js/main.js
+++ JS/phaser/baby animals/01-empty game/js/main.js
@@ -1,49 +1,45 @@
-/* Game Setup:
-- Create an empty game with Phaser.Game
-- The code of the game goes into a State
-- Some of the main methods of State are:
-    preload
-    create
-    update
-
-Load the images in the preload method: this.load.image('myImage','assets/images/file.png')
-
-Create a new Sprite to show the image on the screen: this.background = this.game.add.sprite(0,0, 'myImage')
-
-Coordinates start on the top-left of the screen
-*/
-
-// physical pixel size of game && if browser doesn't have WEBGL, it will default to canvas
+// Load the images in the preload method: this.load.image('myImage','assets/images/file.png')
+// Create a new Sprite to show the image on the screen: this.background = this.game.add.sprite(0,0, 'myImage')
+// Coordinates start on the top-left of the screen
+// Physical pixel size of game && if browser doesn't have WEBGL, it will default to canvas
+// This game is going to be a single state game a state is a JS object with a preload method where all assets are loaded once the game is loaded, the create method is called when create is called, assets can be quickly accessed
 
 var game = new Phaser.Game(640, 360, Phaser.AUTO);
-
-// this game is going to be a single state game
-// a state is a JS object with a preload method where all assets are loaded
-// once the game is loaded, the create method is called
-// when create is called, assets can be quickly accessed
-
 var GameState = {
     preload: function(){
 
-/* this: current object we're in // load: loader object // image: duh // a key eg: 'background'
-    specify the path after the key, */
-
+// this, well it loads the images:
     this.load.image('background', 'assets/images/background.png') 
+    this.load.image('alien', 'assets/images/alien.png')
+    this.load.image('mulder', 'assets/images/mulder.png') 
+    this.load.image('scully', 'assets/images/scully.png') 
+    this.load.image('skinner', 'assets/images/skinner.png') 
     },
-    
+  
+// to center an image, anchor it with a 0.5 y and x axis 0.5, 0.5 or just 0.5 alone
+// to specify scale, use scale.setTo() < put number inside
+// you can scale an image on X and Y by specifying two numbers 
+// to flip, you can set a negative number in scale and it will flip on X or Y
+// to rotate, use .angle, 0 is default eg, -45;
     create: function() {
-        this.background = this.game.add.sprite(0, 0, 'background') // always gives access to the main game object // add coordinates from top left corner // in this case places the background
+        this.background = this.game.add.sprite(0, 0, 'background') 
+        this.alien = this.game.add.sprite (this.game.world.centerX, this.game.world.centerY, 'alien');
+        this.alien.anchor.setTo(0.5);
+//        this.alien.scale.setTo(0.5); 
+//        this.alien.angle = -45;
+
+        
+// Always gives access to the main game object // add coordinates from top left corner // in this case places the background for you
     },
 
-    update: function() { // update is used many times per second as long as the game is going, a listener
+// Update is used many times per second as long as the game is going, a listener
+    update: function() { 
+//        this.alien.angle += 0.5; to rotate slowly
     }
 };
 
-// after creating a state, you have to add it to the game
-
+// After creating a state, you have to add it to the game
 game.state.add('GameState', GameState);
 
-// launch the game
-
+// Launch the game
 game.state.start('GameState');
-
