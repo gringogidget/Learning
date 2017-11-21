7aec8a99cafded3584eb5ec62e8d61a7c47b391e
Kristina Groeger <kristinagroeger@gmail.com>
2017-11-21 18:24:27 -0500

    🇬🇩

--- JS/phaser/virtual pet/js/main.js
+++ JS/phaser/virtual pet/js/main.js
@@ -3,10 +3,10 @@ var GameState = {
 
   //initiate some game-level settings
   init: function() {
-  	//scaling options
-  	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-  	this.scale.pageAlignHorizontally = true;
-  	this.scale.pageAlignVertically = true;
+    //scaling options
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
   },
   //load the game assets before the game starts
   preload: function() {
@@ -20,20 +20,53 @@ var GameState = {
   },
   //executed after everything is loaded
   create: function() {      
-  	this.background = this.game.add.sprite(0, 0, 'backyard');
+    this.background = this.game.add.sprite(0, 0, 'backyard');
 
-  	this.pet = this.game.add.sprite(100, 400, 'pet');
-  	this.pet.anchor.setTo(0.5);
+    this.pet = this.game.add.sprite(100, 400, 'pet');
+    this.pet.anchor.setTo(0.5);
 
-  	//custom properties
-  	this.pet.customParams = {health: 100, fun: 100};
+    //custom properties
+    this.pet.customParams = {health: 100, fun: 100};
 
-  	this.apple = this.game.add.sprite(72, 570, 'apple');
-  	this.candy = this.game.add.sprite(144, 570, 'candy');
-  	this.toy = this.game.add.sprite(216, 570, 'toy');
-  	this.rotate = this.game.add.sprite(288, 570, 'rotate');
+    //draggable pet
+    this.pet.inputEnabled = true;
+    this.pet.input.enableDrag();
+
+    this.apple = this.game.add.sprite(72, 570, 'apple');
+    this.apple.anchor.setTo(0.5);
+    this.apple.inputEnabled = true;
+    this.apple.customParams = {health: 20};
+    this.apple.events.onInputDown.add(this.pickItem, this);
+
+    this.candy = this.game.add.sprite(144, 570, 'candy');
+    this.candy.anchor.setTo(0.5);
+    this.candy.inputEnabled = true;
+    this.candy.customParams = {health: -10, fun: 10};
+    this.candy.events.onInputDown.add(this.pickItem, this);
+
+    this.toy = this.game.add.sprite(216, 570, 'toy');
+    this.toy.anchor.setTo(0.5);
+    this.toy.inputEnabled = true;
+    this.toy.customParams = {fun: 20};
+    this.toy.events.onInputDown.add(this.pickItem, this);
+
+    this.rotate = this.game.add.sprite(288, 570, 'rotate');
+    this.rotate.anchor.setTo(0.5);
+    this.rotate.inputEnabled = true;
+    this.rotate.events.onInputDown.add(this.rotatePet, this);
+
+    this.buttons = [this.apple, this.candy, this.toy, this.rotate];
+
+    //nothing is selected
+    this.selectedItem = null;
 
   },
+  pickItem: function(sprite, event) {
+    console.log('pick item');
+  },
+  rotatePet: function(sprite, event) {
+    console.log('rotating..');
+  }
 
   
 };
--- JS/phaser/virtual pet/readme.md
+++ JS/phaser/virtual pet/readme.md
@@ -1,3 +1,6 @@
 - Use the `init` method to define game settings
 - Spritesheets have a `margin` around them, and a `spacing` between frames
 - Keep variables in the `this` context to access them from different methods
+
+- To enable input on a sprite set `inputEnabled` to `true`
+- to enable dragging first enable input, then call `enableDrag()`
