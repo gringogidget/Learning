406aaab7d811f5689ff93ff15538b0d5829dc7da
Kristina Groeger <kristinagroeger@gmail.com>
2017-11-29 14:39:22 -0500

    🚌

--- JS/phaser/kong/js/main.js
+++ JS/phaser/kong/js/main.js
@@ -30,6 +30,8 @@ var GameState = {
 
     this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 28, 30, 5, 1, 1);    
     this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 2, 1, 1);      
+  
+    this.load.text('level', 'assets/data/level.json');
   },
   //executed after everything is loaded
   create: function() {    
@@ -39,17 +41,15 @@ var GameState = {
     this.ground.body.allowGravity = false;
     this.ground.body.immovable = true;
 
-    var platformData = [
-      {"x": 0, "y": 430},
-      {"x": 45, "y": 560},
-      {"x": 90, "y": 290},
-      {"x": 0, "y": 140}
-    ];
+    //parse the file
+    this.levelData = JSON.parse(this.game.cache.getText('level'));
+
+    console.log(this.levelData);
 
     this.platforms = this.add.group();
     this.platforms.enableBody = true;
 
-    platformData.forEach(function(element){
+    this.levelData.platformData.forEach(function(element){
       this.platforms.create(element.x, element.y, 'platform');
     }, this);
 
@@ -58,7 +58,7 @@ var GameState = {
 
     
     //create player
-    this.player = this.add.sprite(10, 545, 'player', 3);
+    this.player = this.add.sprite(this.levelData.playerStart.x, this.levelData.playerStart.y, 'player', 3);
     this.player.anchor.setTo(0.5);
     this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
     this.game.physics.arcade.enable(this.player);
@@ -76,9 +76,18 @@ var GameState = {
 
     if(this.cursors.left.isDown || this.player.customParams.isMovingLeft) {
       this.player.body.velocity.x = -this.RUNNING_SPEED;
+      this.player.scale.setTo(1, 1);
+      this.player.play('walking');
     }
     else if(this.cursors.right.isDown || this.player.customParams.isMovingRight) {
       this.player.body.velocity.x = this.RUNNING_SPEED;
+      this.player.scale.setTo(-1, 1);
+      this.player.play('walking');
+    }
+    else {
+      this.player.animations.stop();
+      this.player.frame = 3;
+
     }
 
     if((this.cursors.up.isDown || this.player.customParams.mustJump) && this.player.body.touching.down) {
